8aeaeab52db199da64221cef9557f229575836d5
Byron J Calvete <calvete94@gmail.com>
2024-05-10 12:26:33 +0200

    add: challenge 32, part3

--- css-100-challenge/day32/index.js
+++ css-100-challenge/day32/index.js
@@ -1,5 +1,32 @@
-let numberValue = 0
-
 const number = document.querySelector('.number')
 const minus = document.querySelector('.minus')
 const plus = document.querySelector('.plus')
+
+let numberValue = 0
+
+plus.addEventListener('click', () => {
+  updateNumber(1)
+})
+
+minus.addEventListener('click', () => {
+  updateNumber(-1)
+})
+
+const updateNumber = (value) => {
+  numberValue += value
+
+  number.classList.remove('fadeOutDown', 'fadeOutUp')
+  number.querySelector('span').classList.remove('fadeInUp')
+
+  if (value > 0) {
+    number.querySelector('span').classList.add('fadeOutUp')
+    number.innerHTML = `<span class="fadeInUp">${numberValue}</span>`
+  } else if(numberValue < 0) {
+    numberValue = 0
+    number.querySelector('span').classList.add('fadeOutDown')
+    number.innerHTML = `<span class="fadeInDown">${numberValue}</span>`
+  } else {
+    number.querySelector('span').classList.add('fadeOutDown')
+    number.innerHTML = `<span class="fadeInDown">${numberValue}</span>`
+  }
+}
